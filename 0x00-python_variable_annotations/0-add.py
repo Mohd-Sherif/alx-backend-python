f06f955399833241aae06a10790b3fd95a3b6261
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-10-10 09:31:19 -0700

    Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -7,7 +7,3 @@ and a float `b` as arguments and returns their sum as a float.
 
 def add(a: float, b: float) -> float:
     return a + b
-
-
-print(add(1.11, 2.22) == 1.11 + 2.22)
-print(add.__annotations__)
