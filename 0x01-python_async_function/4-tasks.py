c583517db333313b1bf754faafa95843c2bc56ef
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-10-15 11:35:32 -0700

    add comment in the function

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -13,6 +13,9 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Executes task_wait_random n times.
+    """
     delays = []
     tasks = [task_wait_random(max_delay) for _ in range(n)]
 
