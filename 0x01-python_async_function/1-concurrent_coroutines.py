06ce11c06b9240fd4095bf9a289387a952a84be2
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-10-15 11:19:03 -0700

    Let's execute multiple coroutines at the same time with async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,18 +1,38 @@
 #!/usr/bin/env python3
-'''Task 1's module.
-tmp
-'''
+"""
+Import wait_random from the previous python file
+that you’ve written and write an async routine called
+wait_n that takes in 2 int arguments (in this order)
+n and max_delay. You will spawn wait_random n times
+with the specified max_delay.
+
+wait_n should return the list of all the delays
+(float values). The list of the delays should be in
+ascending order without using sort() because of concurrency.
+"""
+
 import asyncio
 from typing import List
-
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    '''Executes wait_random n times.
-    '''
-    wait_times = await asyncio.gather(
-        *tuple(map(lambda _: wait_random(max_delay), range(n)))
-    )
-    return sorted(wait_times)
+    """
+    Executes wait_random n times.
+    """
+    delays = []
+    tasks = [wait_random(max_delay) for _ in range(n)]
+
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        # Insert each completed delay into the correct position
+        # in the list (insertion sort style)
+        for i, d in enumerate(delays):
+            if delay < d:
+                delays.insert(i, delay)
+                break
+        else:
+            delays.append(delay)
+
+    return delays
